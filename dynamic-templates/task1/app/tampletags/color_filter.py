3256ee7601d7833a8a0e5251cc4fc723b4d257a0
adexcell <46703600+adexcell@users.noreply.github.com>
2019-11-07 22:59:47 +0500

    Add files via upload

---
+++ dynamic-templates/task1/app/tampletags/color_filter.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def pick_color(i):
+    try:
+        res = float(i)
+        if res < 0:
+            return "green"
+        else:
+            if 1.0 < res < 2.0:
+                return "#FCD3D3"
+            elif 2.0 < res < 5.0:
+                return "#F98C8C"
+            elif res > 5.0:
+                return "#FF0000"
+            else:
+                return ""
+    except ValueError:
+        return i
